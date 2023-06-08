7a10dff1fde992d44d4a856940e5fc890019650e
Anthony C <gitanthony@yahoo.com>
2023-06-08 10:40:06 -0400

    1) make it work

--- even-or-odd/even-or-odd.js
+++ even-or-odd/even-or-odd.js
@@ -1,3 +1,23 @@
-const prompt = require("prompt-sync")({ sigint: true });
+'use strict';
+
+// const prompt = require('prompt-sync')({ sigint: true });
+
+let myArray = [1, 1, 2, 3, 4, 5, 5, 5];
 
 // Write Code here
+const separateEvensOdds = arrayNums => {
+    let evenNums = [],
+        oddNums = [];
+
+    arrayNums.forEach(num => {
+        if (num % 2 == 0) evenNums.push(num);
+        else oddNums.push(num);
+    });
+
+    console.log(evenNums);
+    console.log(oddNums);
+
+    return;
+};
+
+separateEvensOdds(myArray);
